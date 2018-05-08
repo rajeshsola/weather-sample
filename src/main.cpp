91f7a0457398cfba5ed9db154263c8c57a17ec16
rajeshsola <rajeshsola@gmail.com>
2018-05-08 21:35:18 +0530

    updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,12 @@ int main() {
   CityWeather w1(1001, "pune", 24, 72);
   w1.display();
   WeatherDb mydb(number_of_records);
-  kpit::pnp::MyTime t1;
+  MyTime t1;
   /**
    * TODO: menu operation on mydb like insert,display,
    * find by city code, find by city name,
    * aggregations on temperature,humidity
    */
+  //test changes
   return 0;
 }
