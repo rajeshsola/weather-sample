106f2dd4d5af2e49fd9b27d8a4e3f529cc4f00fa
rajeshsola <rajeshsola@gmail.com>
2018-05-08 22:17:02 +0530

    weatherdb.cpp

--- src/weatherdb.cpp
+++ src/weatherdb.cpp
@@ -9,6 +9,8 @@ WeatherDb::WeatherDb(int len) : m_len(len) {
   // filler code
   w_array = new MyArray<CityWeather>(10);
 }
-WeatherDb::~WeatherDb() { delete w_array; }
+WeatherDb::~WeatherDb() { 
+//delete w_array; 
+}
 
 // TODO: Implement business logic for all the function
