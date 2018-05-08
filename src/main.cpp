e5fc42ba3f5f4711483929619b2aa7597de2e594
rajeshsola <rajeshsola@gmail.com>
2018-05-08 21:37:32 +0530

    updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main() {
   CityWeather w1(1001, "pune", 24, 72);
   w1.display();
   WeatherDb mydb(number_of_records);
-  MyTime t1;
+  //MyTime t1;
   /**
    * TODO: menu operation on mydb like insert,display,
    * find by city code, find by city name,
